e0362d0036da37df674545456ecfd3235afc1258
Luana Lima <luana.developer7@gmail.com>
2025-07-27 10:56:50 -0300

    feat: adiciona exercicio 9 na pasta manipulando_dados

---
+++ manipulando_dados/exercicio9.py
@@ -0,0 +1,4 @@
+base = int(input("Insira um número para base da exponenciação: "))
+expoente = int(input("Insira um número para ser o expoente da base: "))
+exponenciacao = base ** expoente
+print(f"{base}^{expoente} = {exponenciacao}")
