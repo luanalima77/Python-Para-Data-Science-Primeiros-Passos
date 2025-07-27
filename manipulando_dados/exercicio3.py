90bcb4be19f4bfd5cf33f0a8027693460956fcf5
Luana Lima <luana.developer7@gmail.com>
2025-07-27 10:41:04 -0300

    feat: adiciona exercicio 3 na pasta manipulando_dados

---
+++ manipulando_dados/exercicio3.py
@@ -0,0 +1,4 @@
+nome_usuario = input("Por favor, digite seu nome: ")
+idade_usuario = int(input("Por favor, digite sua idade: "))
+altura_usuario = float(input("Por favor, digite sua altura em metros: "))
+print(f"Olá, {nome_usuario}, você tem {idade_usuario} anos e e mede {altura_usuario} metros!")
