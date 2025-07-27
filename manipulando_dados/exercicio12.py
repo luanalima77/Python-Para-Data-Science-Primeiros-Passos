fca55600965ccbcf03b36951c7956e6eace6191d
Luana Lima <luana.developer7@gmail.com>
2025-07-27 11:02:25 -0300

    feat: adiciona exercicio 12 na pasta manipulando_dados

---
+++ manipulando_dados/exercicio12.py
@@ -0,0 +1,6 @@
+n1 = int(input("Insira o primeiro número: "))
+n2 = int(input("Insira o segundo número: "))
+n3 = int(input("Insira o terceiro número: "))
+
+media = (n1 + n2 + n3)/3
+print(f"A média dos valores digitados é: {media}")
