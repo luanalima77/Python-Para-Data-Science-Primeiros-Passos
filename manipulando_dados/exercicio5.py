a7697007454bf4916026923af90efe2dd6446b16
Luana Lima <luana.developer7@gmail.com>
2025-07-27 10:45:08 -0300

    feat: adiciona exercicio 5 na pasta manipulando_dados

---
+++ manipulando_dados/exercicio5.py
@@ -0,0 +1,5 @@
+n1 = int(input("Insira o primeiro número: "))
+n2 = int(input("Insira o segundo número: "))
+n3 = int(input("Insira o terceiro número: "))
+soma = n1 + n2 + n3
+print(f"{n1} + {n2} + {n3} = {soma}")
